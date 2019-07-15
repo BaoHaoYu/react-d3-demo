f5d3465fd4535de3f05896ccecac1c8f489b1a1a
bhy <450215151@qq.com>
2019-07-15 11:52:34 +0800

    加入面积图测试

--- stories/line/d3-demo.tsx
+++ stories/line/d3-demo.tsx
@@ -1,18 +1,19 @@
-import { curveCatmullRom, curveMonotoneX, line, select } from 'd3'
+import { area, curveCatmullRom, curveMonotoneX, line, select } from 'd3'
 import { max } from 'd3-array'
 import { axisBottom, axisLeft } from 'd3-axis'
-import { scaleBand, scaleLinear } from 'd3-scale'
+import * as d3scale from 'd3-scale'
+import $ from 'jquery'
+import { isEqual } from 'lodash-es'
 import * as React from 'react'
-
-
 interface IProps {
   curve: any
   paddingInner: number
+  paddingOuter: number
 }
 
 const listcurve = {
-  '0' : curveMonotoneX,
-  '1' : curveCatmullRom
+  '0': curveMonotoneX,
+  '1': curveCatmullRom,
 }
 
 // https://bl.ocks.org/mbostock/431a331294d2b5ddd33f947cf4c81319
@@ -30,16 +31,16 @@ export class D3Demo extends React.Component<IProps> {
    * com
    */
   public componentWillReceiveProps(next: IProps) {
-    if (this.props.curve !== next.curve) {
-      select('#d3svg').empty()
+    if (!isEqual(this.props, next)) {
+      this.init(next)
     }
   }
 
   /**
    * 促使华
    */
-  public init = () => {
-    select('#d3svg').empty()
+  public init = (props: IProps = this.props) => {
+    $('#d3svg').empty()
     // 数据
     const data: number[] = [820, 932, 400, 400, 1290, 1330, 1320]
     const time: string[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -50,48 +51,45 @@ export class D3Demo extends React.Component<IProps> {
     const margin = { top: 30, right: 20, bottom: 30, left: 40 }
     // scaleLinear为线性比例，根据一个具体的数值计算，计算该数值像素，常用于y轴
     // rangeRound对数据进行保留一位小数，这样防止了ticks模糊
-    const yScale = scaleLinear()
+    const yScale = d3scale
+      .scaleLinear()
       .domain([0, max(data)!])
       .rangeRound([height - margin.top - margin.bottom, margin.left])
 
     // scaleBand为序数比例尺，根据数组的长度，计算每个数组元素在range中的像素位置，长用于x轴
-    const xScale = scaleBand()
+    const xScale = d3scale
+      .scalePoint()
       .domain(time)
-      .rangeRound([0, width - margin.right - margin.left])
-      .paddingInner(1)
-      .paddingOuter(0)
+      .range([0, width - margin.right - margin.left])
 
     // axisLeft axisBottom 会生成具体的svg标签
-    const y = axisLeft(yScale).ticks(5)
-    const x = axisBottom(xScale)
-
-    // 加入y轴，使用transform属性改变位置
-    select('#d3svg')
-      .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-      .call(y)
-
-    // 加入x轴
-    select('#d3svg')
-      .append('g')
-      .attr(
-        'transform',
-        'translate(' + margin.left + ',' + (height - margin.bottom) + ')',
-      )
-      .call(x)
+    const yaxis = axisLeft(yScale).ticks(5)
+    const xaxis = axisBottom(xScale)
 
     // 曲线数据，记得要用比例尺转化为像素数据
     this.lineData = time.map((item, index) => {
       return [xScale(item), yScale(data[index])]
     })
     // 光滑请看 https://github.com/d3/d3-shape/blob/v1.3.4/README.md#curveCatmullRom_alpha
-    const newLine = line().curve(listcurve[this.props.curve])
+    const newLineCreate = area()
+      .x((d) => {
+        return d[0]
+      })
+      // 曲线y0 和曲线y1形成的区域就是面积了
+      .y0((d) => {
+        return d[1]
+      })
+      .y1((d) => {
+        return height - margin.top - margin.bottom
+      })
+      .curve(listcurve[props.curve])
 
+    // 添加曲线
     select('#d3svg')
       .datum(data)
       .append('path')
       .attr('class', 'line')
-      .attr('fill', 'none')
+      .attr('fill', 'steelblue')
       .attr('stroke', 'steelblue')
       .attr('stroke-width', 1.5)
       .attr('stroke-linejoin', 'round')
@@ -101,36 +99,25 @@ export class D3Demo extends React.Component<IProps> {
         `translate(${margin.left + xScale.bandwidth() / 2!},${margin.top})`,
       )
       .attr('d', () => {
-        return newLine(this.lineData)
+        return newLineCreate(this.lineData)
       })
+
+    // 加入y轴，使用transform属性改变位置
+    select('#d3svg')
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+      .call(yaxis)
+
+    // 加入x轴
+    select('#d3svg')
+      .append('g')
+      .attr(
+        'transform',
+        'translate(' + margin.left + ',' + (height - margin.bottom) + ')',
+      )
+      .call(xaxis)
   }
 
-  // 修改path的d属性
-  public changeType0 = () => {
-    const newLine = line()
-
-    select('#d3svg .line').attr('d', () => {
-      return newLine(this.lineData)
-    })
-  }
-
-  // 修改path的d属性
-  public changeType1 = () => {
-    const newLine = line().curve(curveCatmullRom)
-
-    select('#d3svg .line').attr('d', () => {
-      return newLine(this.lineData)
-    })
-  }
-
-  // 修改path的d属性
-  public changeType2 = () => {
-    const newLine = line().curve(curveMonotoneX)
-
-    select('#d3svg .line').attr('d', () => {
-      return newLine(this.lineData)
-    })
-  }
   public render() {
     return (
       <div>
@@ -139,7 +126,7 @@ export class D3Demo extends React.Component<IProps> {
           className="ddd"
           id="d3svg"
           width={700}
-          height={600}
+          height={500}
         />
       </div>
     )
--- stories/line/index.stories.tsx
+++ stories/line/index.stories.tsx
@@ -1,15 +1,33 @@
-import { radios, withKnobs } from '@storybook/addon-knobs'
+import { number, radios, withKnobs } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import { D3Demo } from './d3-demo'
 storiesOf('p1', module)
   .addDecorator(withKnobs)
   .add('demo', () => {
-    const curve = radios('曲线类型:d3.line().curve(???)', { curveMonotoneX: '0', curveCatmullRom: '1' }, '0')
+    const paddingInner = number('scaleBand() - paddingInner()', 0, {
+      range: true,
+      max: 10,
+      min: -10,
+      step: 1,
+    })
+    const paddingOuter = number('scaleBand() - paddingOuter()', 0, {
+      range: true,
+      max: 10,
+      min: -10,
+      step: 0.5,
+    })
+    const curve = radios(
+      '曲线类型:line() - curve()',
+      { curveMonotoneX: '0', curveCatmullRom: '1' },
+      '0',
+    )
     return (
       <div>
-        <D3Demo 
-          curve={curve} 
+        <D3Demo
+          curve={curve}
+          paddingInner={paddingInner}
+          paddingOuter={paddingOuter}
         />
       </div>
     )
